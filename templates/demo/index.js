a977c3033d77997f3f769c9d4e411f7f90544f1f
Ray Deck <ray@raydeck.com>
2017-11-12 17:51:01 -0500

    Fixing js module code

--- templates/demo/index.js
+++ templates/demo/index.js
@@ -2,17 +2,35 @@ import {
   NativeModules,
   NativeEventEmitter
 } from 'react-native';
-const rnswifttemplate = NativeModules.rnswifttemplate;
+const rnswifttemplate_native = NativeModules.rnswifttemplate;
 
-export default testSend(a, b) {
-  console.log(
-    "Constants available immediately: ", 
-    swiftTest.a, 
-    swiftTest.b
-  );
-  const e = new NativeEventEmitter(swiftTest);
-  const s = e.addListener("Hello", (arr) => {
-    console.log("Received a hello event", arr.message);
-  })
-  return swiftTest.addEvent(a, b)
+const rnswifttemplate = {
+  nativeObj: rnswifttemplate_native,
+  a: rnswifttemplate_native.a,
+  b: rnswifttemplate_native.b,
+  startTime: rnswifttemplate_native.startTime,
+  addListener: (cb) => {
+    const e = new NativeEventEmitter(rnswifttemplate_native); 
+    const s = e.addListener("rnswifttemplate", cb); 
+    return s;
+  },
+  addListenerDemo: () => {
+    rnswifttemplate.addListener((arr)=> {
+      console.log("Received a rnswifttemplate event", arr.message);
+    })
+  },
+  emitMessage: (message, delayms) => {
+    if(!delayms) delayms = 0;
+    return rnswifttemplate_native.delayedSend(message, delayms);
+  },
+  demoWithVoid: (obj) => {
+    //Note no point in returning since it is a void function - no promise! 
+    rnswifttemplate_native.demoVoid(obj); 
+  },
+  demoWithPromise: (message) => {
+    //Returns a promise!
+    return rnswifttemplate_native.demo(message)
+  }
 }
+
+export default rnswifttemplate
