ddd0af17d4f56148e5bd20aaadf73fb10f6b6632
Ray Deck <ray@raydeck.com>
2017-11-12 17:50:47 -0500

    Fixing native/swift code

--- templates/demo/ios/rnswifttemplate/rnswifttemplate.m
+++ templates/demo/ios/rnswifttemplate/rnswifttemplate.m
@@ -1,7 +1,9 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(stest, NSObject)
-RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+@interface RCT_EXTERN_MODULE(rnswifttemplate, NSObject)
+RCT_EXTERN_METHOD(demo:(NSString *)name success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(voidDemo:(NSString *)name);
+RCT_EXTERN_METHOD(delayedSend:(message:(NSString *)message ms:(int)ms));
 @end
 
--- templates/demo/ios/rnswifttemplate/rnswifttemplate.swift
+++ templates/demo/ios/rnswifttemplate/rnswifttemplate.swift
@@ -1,28 +1,45 @@
 import Foundation
+//Note that for objective-c (and therefore RN) to see the class you need to give the @objc hint
+//Also, any method exposed to objective-c runtime will also require the hint.
 @objc(rnswifttemplate)
 class rnswifttemplate: RCTEventEmitter {
-    @objc func demo(_ message:String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
-        sendEvent(withName: "Hello",body: [
-            "message": "Take me to your leader"
-        ]);
-        success(["demo message: " + message]);
-    }
-    @objc func delayedSend(message: String, ms:Double) -> Void {
-        delayedSend(["message": message], ms)
-    }
-    @objc func delayedSend(body: [String:Any], ms:Double) -> Void {
-        let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_MSEC)))
-        dispatch_after(delay) {
-            sendEvent(withName: "rnswifttemplate", body: body)
+    @objc func voidDemo(_ dic:[String:Any]) -> Void {
+        //Note that the client will never hear back from this function
+        for thisKey:String in dic.keys {
+            print("Key: " + thisKey);
+            print(dic[thisKey]);
         }
     }
+    //Demonstrate a basic promise-based function in swift
+    @objc func demo(_ message:String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        if(message.count === 0) {
+            print("I did not receive a message");
+            reject(nil, nil, nil);
+        } else {
+            print("I received a message of " + "message");
+            success(["demo message: " + message]);
+        }
+    }
+    //Demonstrate using the event emitter
+    @objc func delayedSend(_ message: String, ms:Int) -> Void {
+        let body = ["message": message];
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(ms)) {
+            self.sendEvent(withName: "rnswifttemplate", body: body)
+        }
+    }
+    //Note that any event name used in sendEvent above needs to be in this array.
     override func supportedEvents() -> [String]! {
         return ["rnswifttemplate"]
     }
+    //Demonstrate setting constants. Note that constants can be (almost) any type, but that this function is only evaluated once, at initialidation
     @objc override func constantsToExport() -> Dictionary<AnyHashable, Any> {
         return [
             "a": "A",
-            "b": "B"
+            "b": "B",
+            "startTime": Date().description
         ];
+    }
+    override class func requiresMainQueueSetup() -> Bool {
+        return false;
     }
 }
