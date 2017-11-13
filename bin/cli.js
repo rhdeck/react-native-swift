f3865fd1b83ab88b2cd3ab4c050dd6ce0b8b9ae4
Ray Deck <ray@raydeck.com>
2017-11-13 13:18:06 -0500

    Fixing client to copy the new template App.js file and improve the help

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 let program = require("commander");
 const makeNewProject = require("../lib/makeNewProject");
+const copyAndReplace = require("../lib/copyAndReplace");
 const fs = require('fs');
 const spawnSync = require("child_process").spawnSync;
 const chdir = require('process').chdir;
@@ -8,6 +9,7 @@ const cwd = require('process').cwd;
 
 program
   .command("init <projectname> [projectpath]")
+  .alias('i')
   .description("Initialize a new swift-based native component project")
   .action(function(projectname, projectpath) {
      if(!projectpath) projectpath = "./" + projectname
@@ -15,7 +17,8 @@ program
    })
 
 program
-  .command("makeapp <appprojectname> <pathToSwiftProjecrt> [appprojectpath]")
+  .command("makeapp <appprojectname> <pathToSwiftProject> [appprojectpath]")
+  .alias("m")
   .description("Create a blank app that adds a swift module to make development easier")
   .action(function(appname, swiftpath, appprojectpath) {
     if(!appprojectpath) appprojectpath = "./" + appname
@@ -34,7 +37,18 @@ program
     spawnSync("yarn", ["add", swiftpath], opts);
     spawnSync("yarn", ["add", "react-native-swift"], opts);
     spawnSync("react-native", ["link"], opts); 
+    const swiftjson = require(swiftpath + '/package.json'); 
+    const swiftprojectname = swiftjson.name
+    copyAndReplace(__dirname + "/../templates/App.js", "./App.js", {
+      "rnswifttemplate": swiftprojectname
+    });
     console.log("Done. To edit your project in xcode, type \"open ios/*xcode*\"\n");     
+  })
+program
+  .command('*')
+  .description('All malformed commands display this help')
+  .action(function(){
+    program.outputHelp(); 
   })
 program
   .parse(process.argv)
