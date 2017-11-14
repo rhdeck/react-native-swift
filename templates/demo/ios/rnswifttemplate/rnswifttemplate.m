aba5e095eb1a9a210b717186e20227ceb7d68f36
Ray Deck <ray@raydeck.com>
2017-11-14 17:13:42 -0500

    Removing templates

--- templates/demo/ios/rnswifttemplate/rnswifttemplate.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface RCT_EXTERN_MODULE(rnswifttemplate, NSObject)
-RCT_EXTERN_METHOD(demo:(NSString *)name success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
-RCT_EXTERN_METHOD(voidDemo:(NSString *)name);
-RCT_EXTERN_METHOD(delayedSend:(NSString *)message ms:(int)ms);
-@end
-
